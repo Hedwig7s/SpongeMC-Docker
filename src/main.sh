17447c72607df9caecb8c405d2e5b6333a6a0f08
Hedwig7s <51961150+Hedwig7s@users.noreply.github.com>
2025-03-18 18:23:17 +0100

    Better params

--- src/main.sh
+++ src/main.sh
@@ -15,7 +15,7 @@
 MCDIR="/home/server"
 MCJAR="$MCDIR/fabric-server-launch.jar"
 MCTEMP="/server_tmp"
-MCARGS="-Xms$MC_RAM_XMS -Xmx$MC_RAM_XMX $MC_PRE_JAR_ARGS -jar $MCJAR $MC_POST_JAR_ARGS"     # -Xms<> -Xmx<> <args> -jar <jar> <args>
+MCARGS="-Xms$MC_RAM_XMS -Xmx$MC_RAM_XMX --add-modules=jdk.incubator.vector -XX:+UseG1GC -XX:+ParallelRefProcEnabled -XX:MaxGCPauseMillis=200 -XX:+UnlockExperimentalVMOptions -XX:+DisableExplicitGC -XX:+AlwaysPreTouch -XX:G1HeapWastePercent=5 -XX:G1MixedGCCountTarget=4 -XX:InitiatingHeapOccupancyPercent=15 -XX:G1MixedGCLiveThresholdPercent=90 -XX:G1RSetUpdatingPauseTimePercent=5 -XX:SurvivorRatio=32 -XX:+PerfDisableSharedMem -XX:MaxTenuringThreshold=1 -Dusing.aikars.flags=https://mcflags.emc.gs -Daikars.new.flags=true -XX:G1NewSizePercent=30 -XX:G1MaxNewSizePercent=40 -XX:G1HeapRegionSize=8M -XX:G1ReservePercent=20 $MC_PRE_JAR_ARGS -jar $MCJAR $MC_POST_JAR_ARGS --nogui"     # -Xms<> -Xmx<> <args> -jar <jar> <args>
 : "${FABRIC_INSTALLVER:=1.0.1}"
 
 cd $MCDIR
